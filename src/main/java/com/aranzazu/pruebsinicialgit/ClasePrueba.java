36d7d92a9baa7610b882fc8286996cbbeca677f1
Aran <aranzazu_90@hotmail.es>
2021-07-21 09:20:04 +0200

    Segundo Comentario

---
+++ src/main/java/com/aranzazu/pruebsinicialgit/ClasePrueba.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aranzazu.pruebsinicialgit;
+
+/**
+ *
+ * @author Aran
+ */
+public class ClasePrueba {
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+}
